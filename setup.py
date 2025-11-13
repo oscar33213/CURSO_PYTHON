5be23d0c502dffa11cdc24e444777bb5113353d8
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2025-11-13 22:28:07 +0100

    ArregloExcepciones

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OperacionesMatematicasBasicas",
-    version="0.1.0",
+    version="0.1.1",
     description="Paquete para realizar operaciones matemáticas básicas y complejas",
     author="Óscar Hidalgo Llopez©",
     author_email="oshill146@gmail.com",
